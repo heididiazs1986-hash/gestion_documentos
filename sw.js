bcbfe1e2465731d80750093fbc3293771e2caed3
heididiazs1986-hash <heidi.diazs1986@gmail.com>
2026-05-12 15:10:29 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "gs-documentos-cache-v8";
+const CACHE_NAME = "gs-documentos-cache-v9";
 const APP_SHELL = [
   "./",
-  "./index.html?v=gsdoc-v8",
-  "./manifest.json?v=gsdoc-v8",
-  "./icon-192.png?v=gsdoc-v8",
-  "./icon-512.png?v=gsdoc-v8",
-  "./icon-180.png?v=gsdoc-v8"
+  "./index.html?v=gsdoc-v9",
+  "./manifest.json?v=gsdoc-v9",
+  "./icon-192.png?v=gsdoc-v9",
+  "./icon-512.png?v=gsdoc-v9",
+  "./icon-180.png?v=gsdoc-v9"
 ];
 
 self.addEventListener("message", (event) => {
@@ -35,6 +35,6 @@ self.addEventListener("fetch", (event) => {
         caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html?v=gsdoc-v8")))
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html?v=gsdoc-v9")))
   );
 });
