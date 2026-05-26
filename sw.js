01d2647effcd691f70f0a36c4ca002898ea255aa
heididiazs1986-hash <heidi.diazs1986@gmail.com>
2026-05-26 17:51:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,6 @@
-const CACHE_NAME = "gs-documentos-cache-v15";
-const APP_SHELL = [
-  "./",
-  "./index.html?v=gsdoc-v15",
-  "./manifest.json?v=gsdoc-v15",
-  "./icon-192.png?v=gsdoc-v15",
-  "./icon-512.png?v=gsdoc-v15",
-  "./icon-180.png?v=gsdoc-v15"
-];
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
-});
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL).catch(() => undefined)));
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin) return;
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html?v=gsdoc-v15")))
-  );
-});
+const CACHE_NAME = "gs-documentos-cache-v17";
+const APP_SHELL = ["./", "./index.html?v=gsdoc-v17", "./manifest.json?v=gsdoc-v17", "./icon-192.png?v=gsdoc-v17", "./icon-512.png?v=gsdoc-v17", "./icon-180.png?v=gsdoc-v17"];
+self.addEventListener("message", e => { if (e.data && e.data.type === "SKIP_WAITING") self.skipWaiting(); });
+self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL).catch(()=>undefined))); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", e => { if(e.request.method !== "GET") return; const url = new URL(e.request.url); if(url.origin !== self.location.origin) return; e.respondWith(fetch(e.request).then(r => { const copy = r.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, copy)); return r; }).catch(() => caches.match(e.request).then(c => c || caches.match("./index.html?v=gsdoc-v17")))); });
